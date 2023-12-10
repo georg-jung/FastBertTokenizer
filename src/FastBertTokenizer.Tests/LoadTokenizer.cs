c4b6fa78b09463f11d5e82a979da79bdcb221642
Georg Jung <git@gjung.com>
2023-12-10 23:58:35 +0100

    Improve test coverage

--- src/FastBertTokenizer.Tests/LoadTokenizer.cs
+++ src/FastBertTokenizer.Tests/LoadTokenizer.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Georg Jung. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Shouldly;
+
 namespace FastBertTokenizer.Tests;
 
 public class LoadTokenizer
@@ -10,10 +12,22 @@ public class LoadTokenizer
     [InlineData("data/bert-base-chinese/tokenizer.json")]
     [InlineData("data/bert-base-multilingual-cased/tokenizer.json")]
     [InlineData("data/bert-base-uncased/tokenizer.json")]
-    public async Task LoadTokenizerFromJson(string path)
+    public async Task LoadTokenizerFromJsonAsync(string path)
     {
         var tokenizer = new BertTokenizer();
         await tokenizer.LoadTokenizerJsonAsync(path);
+    }
+
+    [Theory]
+    [InlineData("data/baai-bge-small-en/tokenizer.json")]
+    [InlineData("data/bert-base-chinese/tokenizer.json")]
+    [InlineData("data/bert-base-multilingual-cased/tokenizer.json")]
+    [InlineData("data/bert-base-uncased/tokenizer.json")]
+    public void LoadTokenizerFromJsonSync(string path)
+    {
+        var stream = File.OpenRead(path);
+        var tokenizer = new BertTokenizer();
+        tokenizer.LoadTokenizerJson(stream);
     }
 
     [Theory]
@@ -21,10 +35,36 @@ public class LoadTokenizer
     [InlineData("data/bert-base-chinese/vocab.txt", true)]
     [InlineData("data/bert-base-multilingual-cased/vocab.txt", true)]
     [InlineData("data/bert-base-uncased/vocab.txt", true)]
-    public async Task LoadTokenizerFromVocab(string path, bool lowercase)
+    public async Task LoadTokenizerFromVocabAsync(string path, bool lowercase)
     {
         var tokenizer = new BertTokenizer();
         await tokenizer.LoadVocabularyAsync(path, lowercase);
+    }
+
+    [Theory]
+    [InlineData("data/baai-bge-small-en/vocab.txt", true)]
+    [InlineData("data/bert-base-chinese/vocab.txt", true)]
+    [InlineData("data/bert-base-multilingual-cased/vocab.txt", true)]
+    [InlineData("data/bert-base-uncased/vocab.txt", true)]
+    public void LoadTokenizerFromVocabSync(string path, bool lowercase)
+    {
+        var stream = File.OpenText(path);
+        var tokenizer = new BertTokenizer();
+        tokenizer.LoadVocabulary(stream, lowercase);
+    }
+
+    [Theory]
+    [InlineData("data/invalid/wrong-version.json")]
+    [InlineData("data/invalid/wrong-model-type.json")]
+    [InlineData("data/invalid/wrong-normalizer.json")]
+    [InlineData("data/invalid/wrong-pretokenizer.json")]
+    [InlineData("data/invalid/dont-strip-accents.json")]
+    [InlineData("data/invalid/dont-handle-chinese-chars.json")]
+    [InlineData("data/invalid/dont-clean-text.json")]
+    public async Task LoadTokenizerFromInvalidJsonAsync(string path)
+    {
+        var tokenizer = new BertTokenizer();
+        await Should.ThrowAsync<ArgumentException>(tokenizer.LoadTokenizerJsonAsync(path));
     }
 
     [Theory]
