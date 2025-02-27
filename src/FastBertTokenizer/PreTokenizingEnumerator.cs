4e0a297598890e3fddc5dd847e016a0c4a0b1b95
Dmitry Kurochkin <kudima03@gmail.com>
2025-02-27 14:15:26 +0300

    Revert "Added full scan of added tokens to choose longest (best) entry."
    This reverts commit 2f4f84d5644b65db2ae2861b40e070fb2d76eca9.

--- src/FastBertTokenizer/PreTokenizingEnumerator.cs
+++ src/FastBertTokenizer/PreTokenizingEnumerator.cs
@@ -158,20 +158,15 @@ internal ref struct PreTokenizingEnumerator
 
     private readonly (int Length, bool Normalize)? StartsWithAddedToken(ReadOnlySpan<char> value)
     {
-        (int Length, bool Normalize)? maxMatch = null;
-
         foreach (var (content, normalize) in _addedTokens.Tokens)
         {
             if (value.StartsWith(content.AsSpan(), normalize ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal))
             {
-                if (maxMatch == null || content.Length > maxMatch.Value.Length)
-                {
-                    maxMatch = (content.Length, normalize);
-                }
+                return (content.Length, normalize);
             }
         }
 
-        return maxMatch;
+        return null;
     }
 
     // AggressiveInlining the methods below does seem to provide a performance boost in the 2-6% ballpark of the overall tokenizer.
